bbcc512259572e84fc3459116a4cf3af7c816021
Matej-Sulfrian <56194456+Matej-Sulfrian@users.noreply.github.com>
2020-01-08 22:10:26 +0100

    Add files via upload

---
+++ Aufgabe_9/script.js
@@ -0,0 +1,22 @@
+var todos = [];
+function numberOfTasks() {
+    document.querySelector(".numberoftasks").innerHTML = todos.length + 1 + " in total";
+}
+function buildTask() {
+    var uncheck = "fa-circle";
+    var circle = "<div class='tasks'><i class='far " + uncheck + "' id='check'></i>";
+    var newtask = document.querySelector(".newtasks").value;
+    todos.push(circle + newtask);
+    document.querySelector(".todos").innerHTML += todos.slice(-1)[0];
+}
+function trash() {
+    console.log(todos);
+    todos.pop();
+    document.querySelector(".todos").innerHTML = todos;
+    document.querySelector(".numberoftasks").innerHTML = todos.length + " in total";
+}
+window.addEventListener("load", function () {
+    document.querySelector(".addTask").addEventListener("click", numberOfTasks);
+    document.querySelector(".addTask").addEventListener("click", buildTask);
+    document.querySelector("#delete").addEventListener("click", trash);
+});
