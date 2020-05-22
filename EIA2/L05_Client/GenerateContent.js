8ea7e71f8d8bb8c0ab389b1f31a781372adf55bd
Matej Sulfrian <EddySagihm@Matejs-MBP.fritz.box>
2020-05-23 01:06:02 +0200

    L05

---
+++ EIA2/L05_Client/GenerateContent.js
@@ -0,0 +1,81 @@
+"use strict";
+var L05_Clien;
+(function (L05_Clien) {
+    function generateContent(_data) {
+        for (let category in _data) {
+            //console.log(category);
+            let items = _data[category];
+            let group = null;
+            switch (category) {
+                case "Einkäufe":
+                    group = createEinkäufe(items, category);
+                    break;
+                case "Geschäfte":
+                    group = createGeschäfte(items, category);
+                    break;
+                case "household":
+                    group = createHaushaltsarbeit(items, category);
+                    break;
+                default:
+                    break;
+            }
+            let space = document.querySelector("#" + category);
+            if (HTMLElement && group) {
+                if (space != null)
+                    space.appendChild(group);
+            }
+        }
+    }
+    L05_Clien.generateContent = generateContent;
+    function createEinkäufe(_items, _category) {
+        let group = document.createElement("datalist");
+        group.id = "article";
+        for (let item of _items) {
+            let option = document.createElement("option");
+            option.setAttribute("name", _category);
+            option.value = item.name;
+            option.setAttribute("unit", item.unit);
+            option.setAttribute("price", item.price.toFixed(2));
+            option.innerHTML = "" + item.name;
+            group.appendChild(option);
+        }
+        return group;
+    }
+    function createGeschäfte(_items, _category) {
+        let group = document.createElement("datalist");
+        group.id = "market";
+        for (let item of _items) {
+            let option = document.createElement("option");
+            option.setAttribute("name", _category);
+            option.value = item.name;
+            option.innerHTML = "" + item.name;
+            group.appendChild(option);
+        }
+        return group;
+    }
+    function createHaushaltsarbeit(_items, _category) {
+        let group = document.createElement("div");
+        for (let item of _items) {
+            let checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.setAttribute("price", item.price.toFixed(2));
+            checkbox.value = item.name;
+            checkbox.name = "ToDo";
+            checkbox.id = item.unit;
+            let lable = document.createElement("label");
+            lable.htmlFor = item.unit;
+            lable.textContent = item.name;
+            let br = document.createElement("br");
+            group.appendChild(checkbox);
+            group.appendChild(lable);
+            group.appendChild(br);
+        }
+        let button = document.createElement("button");
+        button.id = "householdDone";
+        button.type = "button";
+        button.textContent = "Hinzufügen";
+        group.appendChild(button);
+        return group;
+    }
+})(L05_Clien || (L05_Clien = {}));
+//# sourceMappingURL=GenerateContent.js.map
