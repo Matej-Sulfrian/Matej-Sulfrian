59c4ea28b6c6eefde571546599b77bb184d5c514
Matej Sulfrian <EddySagihm@Matejs-MBP.fritz.box>
2020-06-06 20:45:16 +0200

    L07_Database

--- EIA2/L07_Datenbank/server.js
+++ EIA2/L07_Datenbank/server.js
@@ -2,15 +2,33 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Http = require("http");
 const Url = require("url");
+const Mongo = require("mongodb");
 var L07_Datenbank;
 (function (L07_Datenbank) {
-    let server = Http.createServer();
-    console.log(server);
+    let orders;
     let port = process.env.PORT;
     if (port == undefined)
         port = 5001;
-    server.listen(port);
-    server.addListener("request", handleRequest);
+    let databaseUrl = "mongodb+srv://Testuser:halloichhabekeinelustmehr@eia2-e3syb.mongodb.net/Haushaltshilfe?retryWrites=true&w=majority";
+    //let destination: string = "";
+    startServer(port);
+    connectToDatabase(databaseUrl);
+    function startServer(_port /*, _destination: string*/) {
+        let server = Http.createServer();
+        console.log("Server strating on port: " + _port);
+        server.listen(_port);
+        server.addListener("request", handleRequest);
+        /*if (_destination == "local")
+            console.log("Using local databse");
+            databaseUrl = "mongo://27017";*/
+    }
+    async function connectToDatabase(_url) {
+        let options = { useNewUrlParser: true, useUnifiedTopology: true };
+        let mongoClient = new Mongo.MongoClient(_url, options);
+        await mongoClient.connect();
+        orders = mongoClient.db("Haushaltshilfe").collection("Orders");
+        console.log("Database connetion", orders != undefined);
+    }
     function handleRequest(_request, _response) {
         console.log("What's up?");
         _response.setHeader("content-type", "text/html; charset-utf-8");
@@ -20,8 +38,12 @@ var L07_Datenbank;
             for (let key in url.query) {
                 _response.write(key + ": " + url.query[key] + " ");
             }
+            storeOrder(url.query);
         }
         _response.end();
+    }
+    function storeOrder(_order) {
+        orders.insert(_order);
     }
 })(L07_Datenbank = exports.L07_Datenbank || (exports.L07_Datenbank = {}));
 //# sourceMappingURL=server.js.map
--- EIA2/L07_Datenbank/server.ts
+++ EIA2/L07_Datenbank/server.ts
@@ -1,16 +1,44 @@
 import * as Http from "http";
 import * as Url from "url";
+import * as Mongo from "mongodb";
 
 export namespace L07_Datenbank {
-    let server: Http.Server = Http.createServer();
-    console.log(server);
+    interface Order {
+        [type: string]: string | string[];
+    }
+
+    let orders: Mongo.Collection;
 
     let port: number | string | undefined  = process.env.PORT;
     if (port == undefined)
         port = 5001;
 
-    server.listen(port);
-    server.addListener("request", handleRequest);
+    let databaseUrl: string = "mongodb+srv://Testuser:halloichhabekeinelustmehr@eia2-e3syb.mongodb.net/Haushaltshilfe?retryWrites=true&w=majority";
+
+    //let destination: string = "";
+
+    startServer(port, /*destination*/);
+    connectToDatabase(databaseUrl);
+
+    function startServer(_port: number | string /*, _destination: string*/ ): void {
+        let server: Http.Server = Http.createServer();
+        console.log("Server strating on port: " + _port);
+
+        server.listen(_port);
+        server.addListener("request", handleRequest);
+
+        /*if (_destination == "local")
+            console.log("Using local databse");
+            databaseUrl = "mongo://27017";*/
+    }
+
+    async function connectToDatabase (_url: string): Promise<void> {
+        let options: Mongo.MongoClientOptions = {useNewUrlParser: true, useUnifiedTopology: true};
+        let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(_url, options);
+        await mongoClient.connect();
+        orders = mongoClient.db("Haushaltshilfe").collection("Orders");
+        console.log("Database connetion", orders != undefined);
+    }
 
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
         console.log("What's up?");
@@ -23,8 +51,14 @@ export namespace L07_Datenbank {
             for (let key in url.query) {
                 _response.write(key + ": " + url.query[key] + " ");
             }
+
+            storeOrder(url.query);
         }
 
         _response.end();
+    }
+
+    function storeOrder(_order: Order): void {
+        orders.insert(_order);
     }
 }
