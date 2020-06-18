bcc84341fa88c3144232c83e09b3df0746934203
Matej Sulfrian <EddySagihm@Matejs-MBP.fritz.box>
2020-06-18 13:54:27 +0200

    FirstC_Virus_Animation

---
+++ EIA2/L09_Virus_Animation/path.ts
@@ -0,0 +1,13 @@
+        /*let virus: Path2D = new Path2D();
+        let gradient: CanvasGradient = crc2.createRadialGradient(0, 0, 0, 0, 0, 30);
+
+        virus.arc(0, 0, 30, 0, 2 * Math.PI);
+
+        gradient.addColorStop(0, "black");
+        gradient.addColorStop(1, "red");
+
+
+        crc2.save();
+        crc2.translate(_position.x, _position.y);
+        crc2.fillStyle = gradient;
+        crc2.fill(virus);*/
